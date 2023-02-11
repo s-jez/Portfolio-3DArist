0cb4d74eaebc3d7e65580b4d9035dccd693a92a8
s-jez <stanislawjez7@gmail.com>
2023-02-11 19:04:40 +0100

    add shadow on icons.

--- src/components/Main/MainSection.tsx
+++ src/components/Main/MainSection.tsx
@@ -15,40 +15,42 @@ const MainSection = () => {
             <span>I</span>
             <span>!</span>
           </div>
-          <h1>I am Wiktoria</h1>
+          <div>
+            <h1>I am Wiktoria</h1>
+          </div>
           <div className={styles["main__artist"]}>
             <span>3D </span>
             <span>ARTIST</span>
           </div>
+          <div className={styles["main__socials"]}>
+            <a
+              href="https://www.linkedin.com/in/wiktoria-sk%C3%B3rek/"
+              target="_blank"
+              rel="noreferrer"
+              className={styles["main__social"]}
+            >
+              <img src={linkedin} width={80} alt="linkedin" />
+            </a>
+            <a
+              href="https://www.instagram.com/kosiawr/"
+              rel="noreferrer"
+              target="_blank"
+              className={styles["main__social"]}
+            >
+              <img src={instagram} width={80} alt="instagram" />
+            </a>
+            <a
+              href="https://www.artstation.com/wiktoria_skorek"
+              rel="noreferrer"
+              target="_blank"
+              className={styles["main__social"]}
+            >
+              <img src={artstation} width={80} alt="artstation" />
+            </a>
+          </div>
         </div>
         <div className={styles["main__frame"]}>
           <img src={iframe} width={300} alt="gallery frame" />
-        </div>
-        <div className={styles["main__socials"]}>
-          <a
-            href="https://www.linkedin.com/in/wiktoria-sk%C3%B3rek/"
-            target="_blank"
-            rel="noreferrer"
-            className={styles["main__social"]}
-          >
-            <img src={linkedin} width={80} alt="linkedin" />
-          </a>
-          <a
-            href="https://www.instagram.com/kosiawr/"
-            rel="noreferrer"
-            target="_blank"
-            className={styles["main__social"]}
-          >
-            <img src={instagram} width={80} alt="instagram" />
-          </a>
-          <a
-            href="https://www.artstation.com/wiktoria_skorek"
-            rel="noreferrer"
-            target="_blank"
-            className={styles["main__social"]}
-          >
-            <img src={artstation} width={80} alt="artstation" />
-          </a>
         </div>
       </div>
     </div>
