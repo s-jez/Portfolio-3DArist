030fb048e791cf25449ebc5485c6095c21263d5c
s-jez <stanislawjez7@gmail.com>
2023-02-13 16:41:29 +0100

    add h2 tag.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import ArtSection from "./components/Sections/Art/ArtSection";
 import MainSection from "./components/Main/MainSection";
 import FormSection from "./components/Form/FormSection";
 import SkillsSection from "./components/Skills/SkillsSection";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App" id="App">
