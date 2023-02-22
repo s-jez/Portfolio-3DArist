c1b0919b355ddf9e2c40bf14c5afad815238a62a
s-jez <stanislawjez7@gmail.com>
2023-02-22 22:04:50 +0100

    Add full card content to page.

--- src/pages/Blog/Card/BlogCard.tsx
+++ src/pages/Blog/Card/BlogCard.tsx
@@ -1,21 +1,48 @@
 import styles from "./BlogCard.module.css";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BlogPost } from "../utils/blog";
 
 const BlogCard = ({ id, image, title, date, description }: BlogPost) => {
+  const [isNavigating, setIsNavigating] = useState(false);
+
   const navigate = useNavigate();
   const handleClick = (id: number) => {
+    setIsNavigating(true);
     navigate(`/blog/post/${id}`);
   };
   return (
-    <div className={styles["card"]} key={id} onClick={() => handleClick(id)}>
-      <img src={image} className={styles["card__img"]} alt={title} />
-      <h3 className={styles["card__title"]}>{title}</h3>
-      <div className={styles["card__article"]}>
-        <span className={styles["card__date"]}>{date}</span>
-        <p className={styles["card__description"]}>{description}</p>
-      </div>
-    </div>
+    <>
+      {!isNavigating ? (
+        <div
+          className={styles["card"]}
+          key={id}
+          onClick={() => handleClick(id)}
+        >
+          <img src={image} className={styles["card__img"]} alt={title} />
+          <div>
+            <h3 className={styles["card__title"]}>{title}</h3>
+            <div className={styles["card__article"]}>
+              <span className={styles["card__date"]}>{date}</span>
+              <p className={styles["card__description"]}>{description}</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={styles["full-card"]}
+          key={id}
+          onClick={() => handleClick(id)}
+        >
+          <img src={image} className={styles["full-card__img"]} alt={title} />
+          <div className={styles["full-card__article"]}>
+            <h3 className={styles["card__title"]}>{title}</h3>
+            <span className={styles["card__date"]}>{date}</span>
+            <p className={styles["card__description"]}>{description}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
