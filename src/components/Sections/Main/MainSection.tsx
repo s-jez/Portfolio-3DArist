f86680b21c9a1ae5e7dc3e8ee7df917186359c53
s-jez <stanislawjez7@gmail.com>
2023-03-19 12:56:19 +0100

    [fix]: styles main section.

--- src/components/Sections/Main/MainSection.tsx
+++ src/components/Sections/Main/MainSection.tsx
@@ -20,34 +20,36 @@ const MainSection = () => {
             <h1> {t("home.header.title")}</h1>
           </div>
           <div className={styles["main__artist"]}>
-            <span>3D </span>
-            <span>ARTIST</span>
-          </div>
-          <div className={styles["main__socials"]}>
-            <a
-              href="https://www.linkedin.com/in/wiktoria-sk%C3%B3rek/"
-              target="_blank"
-              rel="noreferrer"
-              className={styles["main__social"]}
-            >
-              <img src={linkedin} width={74} height={74} alt="linkedin" />
-            </a>
-            <a
-              href="https://www.instagram.com/kosiawr/"
-              rel="noreferrer"
-              target="_blank"
-              className={styles["main__social"]}
-            >
-              <img src={instagram} width={74} height={74} alt="instagram" />
-            </a>
-            <a
-              href="https://www.artstation.com/wiktoria_skorek"
-              rel="noreferrer"
-              target="_blank"
-              className={styles["main__social"]}
-            >
-              <img src={artstation} width={74} height={74} alt="artstation" />
-            </a>
+            <div className={styles["artist"]}>
+              <span>3D </span>
+              <span>ARTIST</span>
+            </div>
+            <div className={styles["main__socials"]}>
+              <a
+                href="https://www.linkedin.com/in/wiktoria-sk%C3%B3rek/"
+                target="_blank"
+                rel="noreferrer"
+                className={styles["main__social"]}
+              >
+                <img src={linkedin} width={74} height={74} alt="linkedin" />
+              </a>
+              <a
+                href="https://www.instagram.com/kosiawr/"
+                rel="noreferrer"
+                target="_blank"
+                className={styles["main__social"]}
+              >
+                <img src={instagram} width={74} height={74} alt="instagram" />
+              </a>
+              <a
+                href="https://www.artstation.com/wiktoria_skorek"
+                rel="noreferrer"
+                target="_blank"
+                className={styles["main__social"]}
+              >
+                <img src={artstation} width={74} height={74} alt="artstation" />
+              </a>
+            </div>
           </div>
         </div>
         <div className={styles["main__frame"]}>
